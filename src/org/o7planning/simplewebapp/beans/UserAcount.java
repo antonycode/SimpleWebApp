9c8971bf8795d582a7bed79100c7fda430289cbc
thanhhai1509 <thanhhai@Thanhs-Mac-mini.local>
2015-10-06 09:46:44 +0700

    add models: Product vs UserAccount

---
+++ src/org/o7planning/simplewebapp/beans/UserAcount.java
@@ -0,0 +1,39 @@
+package org.o7planning.simplewebapp.beans;
+
+public class UserAcount {
+	public static final String GENDER_MALE = "M";
+	public static final String GENDER_FEMALE = "F";
+	
+	private String userName;
+	private String password;
+	private String gender;
+	
+	public UserAcount() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
